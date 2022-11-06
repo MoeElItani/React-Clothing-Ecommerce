6ffd1b92e2541f862362c8aa0d584c36e71fbf77
MoeElItani <mhmd98itani@gmail.com>
2022-11-06 22:24:46 +0200

    edited login

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useState, useContext } from 'react'
 import {
-  createAuthUserWithEmailAndPassword,
-  createUserDocumentFromAuth,
-  signInWithGooglePopup,
-  signInAuthUserWithEmailAndPassword
+   createAuthUserWithEmailAndPassword,
+   createUserDocumentFromAuth,
+   signInWithGooglePopup,
+   signInAuthUserWithEmailAndPassword,
 } from '../../utils/firebase/firebase.utils'
 import FormInput from '../form-input/form-input.components'
 import './sign-in.styles.scss'
@@ -12,98 +12,106 @@ import Button from '../button/button.component'
 import { UserContext } from '../../contexts/user.context'
 
 const defaultFormFields = {
-  email: '',
-  password: '',
+   email: '',
+   password: '',
 }
 
 const SignInForm = () => {
-  const [ formFields, setFormFields ] = useState(defaultFormFields);
-  const { email, password } = formFields;
+   const [formFields, setFormFields] = useState(
+      defaultFormFields
+   )
+   const { email, password } = formFields
 
-  const {setCurrentUser}= useContext(UserContext);
+   const { setCurrentUser } = useContext(UserContext)
 
-  const resetFormFields = () => {
-    setFormFields(defaultFormFields);
-  }
+   const resetFormFields = () => {
+      setFormFields(defaultFormFields)
+   }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+   const handleSubmit = async (e) => {
+      e.preventDefault()
 
-    try {
-      const {user} = await signInAuthUserWithEmailAndPassword(email, password)
-      setCurrentUser(user);
+      try {
+         const { user } =
+            await signInAuthUserWithEmailAndPassword(
+               email,
+               password
+            )
+         setCurrentUser(user)
+        resetFormFields()
+        
+         alert(`Welcome Back ${user.email}`)
+      } catch (err) {
+         switch (err.code) {
+            case 'auth/wrong-password':
+               alert('Incorrect Password')
+               break
 
-      resetFormFields();
-    } catch (err) {
-      switch (err.code) {
-        case 'auth/wrong-password':
-          alert('Incorrect Password')
-          break;
+            case 'auth/user-not-found':
+               alert('User not found')
+               break
 
-        case 'auth/user-not-found':
-          alert('User not found')
-          break;
+            case 'auth/too-many-requests':
+               alert(
+                  'Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.'
+               )
+               break
 
-        case 'auth/too-many-requests':
-          alert('Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.')
-          break;
-
-        default: console.log(err);
-          break;
+            default:
+               console.log(err)
+               break
+         }
       }
-    }
-  }
+   }
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormFields({ ...formFields, [ name ]: value })
-  }
+   const handleChange = (e) => {
+      const { name, value } = e.target
+      setFormFields({ ...formFields, [name]: value })
+   }
 
-  const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-  }
+   const signInWithGoogle = async () => {
+      const { user } = await signInWithGooglePopup()
+   }
 
-  return (
-    <div className='sign-up-container'>
-      <h2>Already have an account?</h2>
-      <span>Sign ip with your email and password</span>
-      <form onSubmit={handleSubmit}>
+   return (
+      <div className='sign-up-container'>
+         <h2>Already have an account?</h2>
+         <span>Sign ip with your email and password</span>
+         <form onSubmit={handleSubmit}>
+            <FormInput
+               label='Email'
+               inputOptions={{
+                  required: true,
+                  type: 'email',
+                  onChange: handleChange,
+                  name: 'email',
+                  value: email,
+               }}
+            />
+            <FormInput
+               label='Password'
+               inputOptions={{
+                  required: true,
+                  type: 'password',
+                  onChange: handleChange,
+                  name: 'password',
+                  value: password,
+               }}
+            />
 
-        <FormInput label='Email'
-          inputOptions={
-            {
-              required: true,
-              type: 'email',
-              onChange: handleChange,
-              name: 'email',
-              value: email
-            }
-          }
-        />
-        <FormInput label='Password'
-          inputOptions={
-            {
-              required: true,
-              type: 'password',
-              onChange: handleChange,
-              name: 'password',
-              value: password
-            }
-          }
-        />
-
-        <div className='buttons-container'>
-          <Button type='submit'>Sign In</Button>
-          <Button
-            type='button'
-            buttonType='google'
-            onClick={signInWithGoogle}>
-            Google sign in
-          </Button>
-        </div>
-      </form>
-    </div>
-  )
+            <div className='buttons-container'>
+               <Button type='submit'>Sign In</Button>
+               <Button
+                  type='button'
+                  buttonType='google'
+                  onClick={signInWithGoogle}
+               >
+                  Google sign in
+               </Button>
+            </div>
+         </form>
+      </div>
+   )
 }
 
 export default SignInForm
