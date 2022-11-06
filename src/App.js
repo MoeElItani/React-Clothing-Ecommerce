7b198c97a2d218a0f8998239bc56aa12f5cfb55c
MoeElItani <mhmd98itani@gmail.com>
2022-11-06 22:38:22 +0200

    edited shop

--- src/App.js
+++ src/App.js
@@ -7,25 +7,39 @@ import Cart from './routes/cart/cart.component'
 import Authentication from './routes/authentication/authentication.component'
 
 const Shop = () => {
-  return <h1>SHOP</h1>
+   return (
+      <>
+         <h1>SHOP</h1>
+         <h3>
+            This Project was made as a practice for
+            authentication with firebase and most components
+            are empty!
+         </h3>
+      </>
+   )
 }
 
 const App = () => {
-
-  return (
-    <Routes>
-      <Route path='/' element={<Navigation />} >
-        <Route path='/' element={<Footer />} >
-          {/* index is shorthand to index={true}, it tells the browser that this is the homepage */}
-          <Route index element={<Home />} />
-          <Route path='/shop' element={<Shop />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/auth' element={<Authentication />} />
-          <Route path='/cart' element={<Cart />} />
-        </Route>
-      </Route>
-    </Routes>
-  );
+   return (
+      <Routes>
+         <Route path='/' element={<Navigation />}>
+            <Route path='/' element={<Footer />}>
+               {/* index is shorthand to index={true}, it tells the browser that this is the homepage */}
+               <Route index element={<Home />} />
+               <Route path='/shop' element={<Shop />} />
+               <Route
+                  path='/contact'
+                  element={<Contact />}
+               />
+               <Route
+                  path='/auth'
+                  element={<Authentication />}
+               />
+               <Route path='/cart' element={<Cart />} />
+            </Route>
+         </Route>
+      </Routes>
+   )
 }
 
-export default App;
+export default App
